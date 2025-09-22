e94beaeb3cda0ac822f40470a61df650bd80791d
Tauheed jahan <tauheedjahan07@gmail.com>
2025-09-22 16:03:35 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,30 +13,32 @@ const About = () => {
           </p>
         </div>
 
-        <p className="text-xl mt-5 text-gray-300">
+        <p className="text-xl mt-5">
           I’m <strong>Tauheed Jahan (TJ)</strong> — a <strong>Full Stack Developer & UI/UX Designer</strong> with 4+ years of experience building responsive websites and data-driven applications. My journey started with front-end design, and over time I expanded into full stack development to deliver end-to-end digital solutions.
         </p>
 
         <br />
 
-        <p className="text-xl text-gray-300">
+        <p className="text-xl ">
           <strong>What I Do:</strong><br/>
-          Frontend: React, Next.js, Tailwind CSS, JavaScript, HTML, CSS<br/>
-          Backend: Node.js, Express.js, REST APIs, MongoDB/MySQL<br/>
-          Design: UI/UX with Figma, Adobe Creative Suite, minimalistic branding<br/>
-          Other: WordPress, SEO, GitHub, deployment & version control
+         Frontend: React, Next.js, Tailwind CSS, JavaScript, HTML, CSS<br/>
+         Backend: Node.js, Express.js, REST APIs, MongoDB/MySQL<br/>
+         Design: UI/UX with Figma, Adobe Creative Suite, minimalistic branding<br/>
+         Data Analytics: Python, Excel, Google Sheets, Data Visualization<br/>
+         Other: WordPress, SEO, GitHub, deployment & version control
+
         </p>
 
         <br />
 
-        <p className="text-xl text-gray-300">
+        <p className="text-xl ">
           <strong>My Approach:</strong><br/>
           I believe in combining clean code with thoughtful design to create products that are fast, scalable, and user-friendly. From custom business websites to full-stack applications, I ensure every project delivers both aesthetic appeal and practical functionality.
         </p>
 
         <br />
 
-        <p className="text-xl text-gray-300">
+        <p className="text-xl">
           <strong>Vision:</strong><br/>
           To help businesses and individuals transform ideas into impactful digital experiences — whether that’s through a modern website, an interactive dashboard, or a complete full-stack solution.
         </p>
