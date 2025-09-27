b032f4b4d762ee084222eace258118f8a794a1eb
Tauheed jahan <tauheedjahan07@gmail.com>
2025-09-27 13:20:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,13 @@ module.exports = {
     },
   },
   plugins: [],
+  safelist: [
+    "sm:text-7xl",
+    "md:w-4/5",
+    "w-3/5",
+    "w-2/5",
+    "text-5xl",
+    "text-6xl",
+    "text-4xl",
+  ],
 };
